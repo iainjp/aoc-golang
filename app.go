e0101f5a35ab9ddfdea62232acebb8a0758d2fd9
iainjp <iainpritchard92@googlemail.com>
2025-11-28 13:55:52 +0000

    Basic boilerplate for CLI interface

--- app.go
+++ app.go
@@ -3,9 +3,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+    "os"
+    "context"
+
+    "github.com/urfave/cli/v3"
 )
 
 
 func main() {
-	fmt.Println("Hello world!")
+	cmd := &cli.Command {
+		Commands : []*cli.Command{
+			{
+				Name: "pull",
+				Usage: "Pull details for given AoC day",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fmt.Println("Pulling ...")
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+    }
 }
