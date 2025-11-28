6311a9ef5f7a2dd0e3374f3e9add32254c317ca9
iainjp <iainpritchard92@googlemail.com>
2025-11-28 21:31:05 +0000

    Configure scraper limits

--- pull.go
+++ pull.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
 	colly "github.com/gocolly/colly/v2"
@@ -29,20 +30,32 @@ func getUrl(year int, day int) string {
 	return fmt.Sprintf("https://adventofcode.com/%d/day/%d", year, day)
 }
 
-func getPage(url string) (string, error) {
+func configureScraper() *colly.Collector {
 	c := colly.NewCollector(
 		colly.UserAgent("aoc-golang-bot"),
 		colly.AllowedDomains("adventofcode.com"),
 		colly.MaxDepth(1),
 	)
 
-	var pageContent string
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*adventofcode.com*",
+		Parallelism: 1,
+		Delay:       1 * time.Second,
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		// TODO validate this exists
 		sessionCookie := os.Getenv("AOC_SESSION_COOKIE")
 		r.Headers.Set("Cookie", "session="+sessionCookie)
 	})
+
+	return c
+}
+
+func getPage(url string) (string, error) {
+	c := configureScraper()
+
+	var pageContent string
 
 	c.OnHTML("article", func(e *colly.HTMLElement) {
 		content, _ := e.DOM.First().Html()
