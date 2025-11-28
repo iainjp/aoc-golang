f6ee8c0d0f25fe6e933deb64069c9ec6aeb86ec6
iainjp <iainpritchard92@googlemail.com>
2025-11-28 14:06:25 +0000

    Add required flags for year and day

--- app.go
+++ app.go
@@ -13,12 +13,30 @@ import (
 
 func main() {
 	cmd := &cli.Command {
+
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:    "year",
+				Aliases: []string{"year"},
+				Usage:   "AoC year",
+				Required: true,
+			},
+			&cli.IntFlag{
+				Name:    "day",
+				Aliases: []string{"day"},
+				Usage:   "AoC day",
+				Required: true,
+			},
+		},
+
 		Commands : []*cli.Command{
 			{
 				Name: "pull",
 				Usage: "Pull details for given AoC day",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
-					fmt.Println("Pulling ...")
+					year := cmd.Int("year")
+					day := cmd.Int("day")
+					fmt.Printf("Pulling details for AoC %d Day %d ...\n", year, day)
 					return nil
 				},
 			},
